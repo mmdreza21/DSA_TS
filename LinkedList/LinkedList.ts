fcee829d8079d234b90f353dbcfca5b1167c275b
mmdreza21 <mrezaj11@live.com>
2025-04-10 21:09:01 +0330

    linked list indexOf deleteLast,deleteFirst added

--- LinkedList/LinkedList.ts
+++ LinkedList/LinkedList.ts
@@ -9,8 +9,12 @@ class ListNode {
 }
 
 class LinkedList {
-  private first: ListNode | null = null;
-  private last: ListNode | null = null;
+  private first: ListNode | undefined = undefined;
+  private last: ListNode | undefined;
+
+  private get isEmpty(): boolean {
+    return !this.first || !this.last;
+  }
 
   /**
    * addLast
@@ -18,29 +22,95 @@ class LinkedList {
    */
   public addLast(value: number) {
     const node = new ListNode(value);
-    if (!this.first) this.first = node;
+    if (this.isEmpty) this.first = node;
     if (this.last) this.last.next = node;
     this.last = node;
   }
 
-  //addFirst
+  /**
+   * addFirst
+   * @param value:number
+   */
+  public addFirst(value: number) {
+    const node = new ListNode(value);
+    if (this.isEmpty) {
+      this.last = node;
+      this.first = node;
+      return;
+    }
+    node.next = this.first;
+    this.first = node;
+  }
 
-/**
- * addFirst
- * @param value
- */
-public addFirst() {
-    
-}
+  /**
+   * deleteFirst: delete the first item in the linkedList
+   */
+  public deleteFirst() {
+    if (this.isEmpty) throw new Error("linkedList is empty!");
+    if (!this.first!.next) {
+      this.first = undefined;
+      this.last = undefined;
+      return;
+    }
+    const first = structuredClone(this.first);
+    this.first!.next = undefined;
+    this.first = first?.next;
+  }
 
-  //deleteFirst
-  //deleteLast
+  /**
+   * indexOf the givin value
+   * @param value:number
+   */
+  public indexOf(value: number): number {
+    if (this.isEmpty) throw new Error("linkedList is empty!");
+    let listVal = this.first?.value;
+    let node = this.first;
+    let counter = 0;
+    while (listVal !== value && node?.next) {
+      node = node?.next;
+      listVal = node?.value;
+
+      counter++;
+    }
+
+    return listVal !== value ? -1 : counter;
+  }
+
+  private getPrevious() {
+    let node = this.first;
+    while (node?.next?.next) {
+      node = node?.next;
+      console.log("mmd");
+    }
+  }
+
+  /**
+   *  delete the last item in the linkedList
+   */
+  public deleteLast() {
+    if (this.isEmpty) throw new Error("linkedList is empty!");
+    let last = structuredClone(this.last);
+    let node = this.first;
+
+    while (node?.next?.next) {
+      node = node?.next;
+      console.log("mmd");
+    }
+    this.last = node;
+    node!.next = undefined;
+  }
+
   //contain
-  // indexOf
 }
 
 const linkedList = new LinkedList();
-linkedList.addLast(1);
-linkedList.addLast(2);
+linkedList.addFirst(2);
+linkedList.addFirst(1);
 linkedList.addLast(3);
+linkedList.addLast(4);
+linkedList.addLast(5);
+
+linkedList.deleteLast();
+
 console.log(linkedList);
+// console.log(linkedList.indexOf(2));
