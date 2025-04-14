c83e070f2ebf07a059c9f5d4dbad4a8911b211f3
mohamadreza <mrezaj11@gmail.com>
2025-04-14 17:06:15 +0330

    chore(stack): uncomment Stack class implementation and add method stubs

--- Stack/Stack.ts
+++ Stack/Stack.ts
@@ -1,3 +1,25 @@
-// export class Stack {
-//   constructor( ) {}
-// }
+export class Stack {
+  constructor() {}
+
+  private array: number[] = [];
+
+  /**
+   * push
+   */
+  public push() {}
+
+  /**
+   * pop
+   */
+  public pop() {}
+
+  /**
+   * peek
+   */
+  public peek() {}
+
+  /**
+   * isEmpty
+   */
+  public isEmpty() {}
+}
