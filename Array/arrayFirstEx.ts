0600f109de9c1ba435c39c9a849c2afc0fe61d9d
mmdreza21 <mrezaj11@live.com>
2025-04-14 07:55:05 +0330

    refactor(array): comment out example usage in CArray class for clarity

--- Array/arrayFirstEx.ts
+++ Array/arrayFirstEx.ts
@@ -130,16 +130,16 @@ class CArray<T extends string | number> {
   }
 }
 
-const arr = new CArray<number>(3);
-arr.insert(1);
-arr.insert(2);
-arr.insert(3);
-arr.insert(4);
-arr.insert(5);
-arr.insert(6);
-arr.insert(7);
+// const arr = new CArray<number>(3);
+// arr.insert(1);
+// arr.insert(2);
+// arr.insert(3);
+// arr.insert(4);
+// arr.insert(5);
+// arr.insert(6);
+// arr.insert(7);
 
-// arr.removeAt(2);
+// // arr.removeAt(2);
 
-arr.print();
-console.log(arr.reverse());
+// arr.print();
+// console.log(arr.reverse());
