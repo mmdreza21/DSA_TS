c9018ae45aa5c3c0591ff4aa865bc55710ed3b8c
mmdreza21 <mrezaj11@live.com>
2025-04-20 22:17:02 +0330

    feat(Stack): make Stack class generic to support multiple data types

--- Stack/Stack.ts
+++ Stack/Stack.ts
@@ -1,12 +1,12 @@
-export class Stack {
-  private stack: number[];
+export class Stack<T> {
+  private stack: T[];
   private size: number = 0;
   private capacity: number;
 
   constructor(size: number) {
     if (size < 0) throw new Error("Size must be positive");
     this.capacity = size;
-    this.stack = new Array<number>(size);
+    this.stack = new Array<T>(size);
   }
   get length() {
     return this.size;
@@ -15,23 +15,23 @@ export class Stack {
     return this.stack.slice(0, this.size);
   }
 
-  public push(item: number) {
+  public push(item: T) {
     if (this.size >= this.capacity)
       throw new Error("stack overFlow -> https://stackoverflow.com/");
     this.stack[this.size++] = item;
   }
 
-  public pop(): number | undefined {
+  public pop(): T | undefined {
     if (this.isEmpty()) return undefined;
     const value = this.stack[--this.size];
     this.stack[this.size] = undefined!; // Optional cleanup
     return value;
   }
 
-  public peek(): number | undefined {
+  public peek(): T | undefined {
     if (this.isEmpty()) return undefined;
-    // return this.stack[this.size -1]
-    return this.stack.at(-1);
+    return this.stack[this.size - 1];
+    // return this.stack.at(-1);
   }
 
   public isEmpty(): boolean {
@@ -48,10 +48,12 @@ export class Stack {
    * @returns min member of the stack
    */
   public min(): number {
+    if (typeof this.stack[0] !== "number")
+      throw new Error("This future is for numeric stack!");
     if (this.isEmpty()) throw new Error("Stack is empty");
-    let min = this.stack[0];
+    let min = this.stack[0] as number;
     for (const e of this.stack) {
-      if (e < min) min = e;
+      if ((e as number) < min) min = e as number;
     }
     return min;
   }
