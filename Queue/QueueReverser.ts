a0d2d75c47f4eadeea6ede6cc8fcd00889577eba
mohamadreza <mrezaj11@gmail.com>
2025-04-21 15:42:32 +0330

    feat(QueueReverser): update reverse method to use generic Queue type and adjust stack size

--- Queue/QueueReverser.ts
+++ Queue/QueueReverser.ts
@@ -2,13 +2,22 @@ import { Stack } from "../Stack/Stack.ts";
 import { Queue } from "./Queue.ts";
 
 export class QueueReverser {
-  public static reverse(queue: Queue): void {
-    queue.print();
+  public static reverse(queue: Queue<any>): void {
     if (queue.isEmpty()) throw new Error("queue is mt");
 
-    const stack = new Stack(3);
+    const stack = new Stack(6);
     while (!queue.isEmpty()) stack.push(queue.dequeue() as number);
 
     while (!stack.isEmpty()) queue.enqueue(stack.pop()!);
+  }
+
+  public static reverseTillK(k: number, queue: Queue<any>) {
+    if (queue.isEmpty()) throw new Error("queue is mt");
+    const stack = new Stack(k);
+
+    while (!stack.isFull()) stack.push(queue.dequeue());
+    console.log(stack.stackContent);
+
+    while (!stack.isEmpty()) queue.enqueue(stack.pop());
   }
 }
