c09e8b465e111d3d3ad0c7616afa9354880bfeb5
mohamadreza <mrezaj11@gmail.com>
2025-04-21 15:42:24 +0330

    feat(LinkedListQueue): implement LinkedListQueue class with basic queue operations

---
+++ Queue/LinkedListQueue.ts
@@ -0,0 +1,40 @@
+import { LinkedList } from "../LinkedList/LinkedList.ts";
+
+export class LinkedListQueue {
+  private queue: LinkedList;
+  private counter: number = 0;
+
+  constructor() {
+    this.queue = new LinkedList();
+  }
+
+  enqueue(item: number): void {
+    this.queue.addLast(item);
+    this.counter++;
+  }
+
+  dequeue(): number | undefined {
+    if (this.counter === 0) throw new Error("Q is empty");
+    const val = this.queue.front?.value;
+    this.queue.deleteFirst();
+    this.counter--;
+    return val;
+  }
+
+  peek(): number | undefined {
+    if (this.counter == 0) return undefined;
+    return this.queue.front?.value;
+  }
+
+  isEmpty(): boolean {
+    return this.counter === 0;
+  }
+
+  get size(): number {
+    return this.counter;
+  }
+
+  print() {
+    console.log("[", this.queue.toArray().join(" -> "), "]");
+  }
+}
