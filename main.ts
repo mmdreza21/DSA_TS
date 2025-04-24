bc49e46d061b292f5c1f43c6c8517f0b4ca24ad3
mmdreza21 <mrezaj11@live.com>
2025-04-24 06:51:23 +0330

    feat(Main): integrate HashTable for key-value storage demonstration

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 import readline from "readline";
 
 import { CharFinder } from "./HashTables/CharFinder.ts";
+import { HashTable } from "./HashTables/HashTable.ts";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,8 +11,12 @@ const rl = readline.createInterface({
 
 class Main {
   constructor() {
-    const f = new CharFinder();
-    console.log(f.FirstRepeatedChar("green apple"));
+    const hT = new HashTable(5);
+    hT.put(3, "mmdreza");
+    hT.put(4, "ali");
+    hT.put(1, "ahad");
+    hT.put(6, "ahmad");
+    hT.print();
   }
 }
 
