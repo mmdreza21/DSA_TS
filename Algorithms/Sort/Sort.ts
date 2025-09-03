091a8730a4e03b4f89bbb4ccd9fd4b6a210d60b6
mmdreza21 <mrezaj11@live.com>
2025-09-03 07:46:46 +0330

    All Files Moved

--- Algorithms/Sort/Sort.ts
+++ Algorithms/Sort/Sort.ts
@@ -1,10 +1,18 @@
 export class Sort {
   public static BobbleSort(array: Array<number>) {
-    for (let index = 0; index < array.length; index++) {
-      const element = array[index];
+    for (let i = 0; i < array.length; i++) {
       for (let j = 0; j < array.length; j++) {
-        const element = array[j];
+        let current = array[i];
+        let next = array[i + 1];
+        if (current > next) {
+          const temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+          console.log("array", array);
+        }
       }
     }
+
+    return array;
   }
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 // main.ts
 import readline from "readline";
-
-import { WeightedGraph } from "./Data_Structure/Graph/WeightedGraph.ts";
+import { Sort } from "./Algorithms/Sort/Sort.ts";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,30 +9,9 @@ const rl = readline.createInterface({
 
 class Main {
   constructor() {
-    const graph = new WeightedGraph();
+    const sort = Sort.BobbleSort([8, 3, 4, 1, 2]);
 
-    graph.addNode("a");
-    graph.addNode("b");
-    graph.addNode("c");
-    graph.addNode("d");
-
-    graph.addEdge("a", "b", 1);
-    graph.addEdge("b", "d", 4);
-    graph.addEdge("c", "d", 5);
-    graph.addEdge("a", "c", 1);
-    graph.addEdge("b", "c", 2);
-    // graph.addEdge("a", "c", 10);
-
-    // console.log(graph.hasCycleMosh("a"));
-    // // graph.removeEdge("a", "b");
-    // // graph.removeNode("a");
-    // // graph.DepthFirstTraverse("d");
-    // console.log("graph node ", [...graph]);
-    console.log("graph edge ", [...graph.getMinimumSpanningTree().edges()]);
-    console.log("graph edge ", [...graph.edges()]);
-    console.log("---------------------------");
-
-    console.log("graph edge ", [...graph.getMinimumSpanningTree()]);
+    console.log(sort);
   }
 }
 
